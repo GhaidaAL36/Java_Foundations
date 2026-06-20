c458fa7c331938bbc7e3c3ed49ab50eec5349f8d
galrahimi <ghaida3638@gmail.com>
2026-06-20 19:32:04 +0300

    some fix

--- Task_List/src/test/java/com/tasl/tasklist/TaskListApplicationTests.java
+++ Task_List/src/test/java/com/tasl/tasklist/TaskListApplicationTests.java
@@ -3,7 +3,6 @@ package com.tasl.tasklist;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class TaskListApplicationTests {
 
     @Test
